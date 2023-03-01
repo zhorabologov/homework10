70356c3c85b699f2c6712f274539abbc56b2b669
Жора <zhorabologov@gmal.com>
2023-03-01 20:46:55 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+
+        int year = 2020;
+        boolean yearLeap = isYearLeap(year);
+        System.out.println(year + " год — високосный год». Если год невисокосный, то:" + yearLeap +  "год — невисокосный год");
     }
+
+
+        public static boolean isYearLeap(int year) {
+            if ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) ;
+            return true;
+        } else {
+        return false
+    }
+
+    }
+
+
+
+
+
+
+
 }
