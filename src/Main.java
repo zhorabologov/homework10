fc47e95cb7705fbfc3ce5544e4f710f23a6e9885
Жора <zhorabologov@gmal.com>
2023-03-21 03:06:02 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,71 @@
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
         // task1
         int year = 2021;
-        boolean yearLeap = isYearLeap (year);
-        //System.out.println (yearLeap + " год — високосный год». Если год невисокосный, то:" + year + "год — невисокосный год");
+        boolean yearLeap = isYearLeap (year) {
+            System.out.println (yearLeap + " год — високосный год». Если год невисокосный, то:" + year + "год — невисокосный год");
+        }
 
         //task2
 
-        determineTypeOC (type 0, clientDeviceYear 2015);
+        determineTypeOC (type 0, clientDeviceYear 2015) {
+        }
+
 
         //task3
         int distance = 95;
         int time = intervalTime (distance);
-        System.out.println ("Потребуется дней: " + time + " срок доставки " + distance);
+        System.out.println ("Потребуется дней: " + time + " срок доставки " + distance) {
+        }
 
 
-        public static boolean isYearLeap(int year){
+        public static boolean isYearLeap(int year) {
             if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
                 return true;
             } else {
                 return false;
             }
+        }
+    }
 
 
-            public static void determineTypeOC(int type, int clientDeviceYear) {
-                int currentYear = LocalDate.now ().getYear ();
+    public static void determineTypeOC(int type, int clientDeviceYear) {
+        int currentYear = LocalDate.now ().getYear ();
 
-                if (type == 0) {
-                    if (clientDeviceYear < currentYear) {
-                        System.out.println ("Установите облегченную версию приложения для iOS по ссылке");
-                    } else {
-                        System.out.println ("Установите стандартную версию на iOS");
-                    }
-
-                } else if (type == 1) {
-                    if (clientDeviceYear < currentYear) {
-                        System.out.println ("Установите облегченную версию приложения для Android по ссылке");
-                    } else {
-                        System.out.println ("Установите стандартную версию на Android");
-                    }
-                }
+        if (type == 0) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println ("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println ("Установите стандартную версию на iOS");
             }
 
-            public static int intervalTime(int distance) {
-                if (distance > 100) {
-                    return -1;
-                }
-                int time = 1;
-                if (distance >= 20) {
-                    ++time;
-                }
-                if (distance >= 60) {
-                    ++time;
-                }
-                if (distance <= 100) {
-                    ++time;
-                }
-                return time;
-
+        } else if (type == 1) {
+            if (clientDeviceYear < currentYear) {
+                System.out.println ("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println ("Установите стандартную версию на Android");
             }
         }
+    }
+
+    public static int intervalTime(int distance) {
+        if (distance > 100) {
+            return -1;
+        }
+        int time = 1;
+        if (distance >= 20) {
+            ++time;
+        }
+        if (distance >= 60) {
+            ++time;
+        }
+        if (distance <= 100) {
+            ++time;
+        }
+        return time;
+
+    }
+}
 }
